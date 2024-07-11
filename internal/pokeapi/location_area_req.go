c349c6d7b1deba4cbc2d68ba0cd2e94932476e20
Vanitas <pfboltryk@gmail.com>
2024-07-11 22:05:42 +0100

    first html request

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,42 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// NOTE:GO HTTP GET request
+func (c *Client) ListLocationAreas() (LocationAreaResponse, error) {
+	endpoint := "/location_area"
+	fullURL := baseUrl + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	if res.StatusCode > 399 {
+		return LocationAreaResponse{}, fmt.Errorf("bad status code: %v", res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	locationAreasResp := LocationAreaResponse{}
+	err = json.Unmarshal(data, &locationAreasResp)
+
+	if err != nil {
+		return LocationAreaResponse{}, err
+	}
+
+	return locationAreasResp, nil
+}
